2bd645758272881aaf6f4bb59994f7b704f14002
SunnyKumar1709 <SunnyKumar1709>
2024-02-27 13:03:06 +0000

    mkmk

--- formula-1 databricks/Notebook Introduction.py
+++ formula-1 databricks/Notebook Introduction.py
@@ -1,13 +1,13 @@
 # Databricks notebook source
-print('Learning databricks')
+
 
 # COMMAND ----------
 
-print('hello sunny')
+
 
 # COMMAND ----------
 
-print('just checking')
+
 
 # COMMAND ----------
 
